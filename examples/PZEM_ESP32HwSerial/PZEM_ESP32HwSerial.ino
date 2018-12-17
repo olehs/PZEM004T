ecb01be2a6bfbb6b33f9fe5b5c70efb2c9ee8843
olehs <olehs@i.ua>
2018-12-17 22:54:00 +0200

    renamed hwserial port variable in ESP32 example
    Just to avoid redeclaration of Serial2.
    See #51

--- examples/PZEM_ESP32HwSerial/PZEM_ESP32HwSerial.ino
+++ examples/PZEM_ESP32HwSerial/PZEM_ESP32HwSerial.ino
@@ -5,8 +5,8 @@
    An example on how to use ESP32 hardware serial with PZEM004T
 */
 
-HardwareSerial Serial2(2);     // Use hwserial UART2 at pins IO-16 (RX2) and IO-17 (TX2)
-PZEM004T pzem(&Serial2);
+HardwareSerial PzemSerial2(2);     // Use hwserial UART2 at pins IO-16 (RX2) and IO-17 (TX2)
+PZEM004T pzem(&PzemSerial2);
 IPAddress ip(192,168,1,1);
 
 void setup() {
